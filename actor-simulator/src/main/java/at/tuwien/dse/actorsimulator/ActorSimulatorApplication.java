a3138ad3c0a181ee1fcfceecad186ccbda3ee67f
Alija Abadzic <alija.abadzic@ibb-as.com>
2020-05-30 17:07:47 +0200

    route.txt

--- actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java
+++ actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java
@@ -1,7 +1,17 @@
 package at.tuwien.dse.actorsimulator;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -11,7 +21,7 @@ import javax.ws.rs.client.ClientBuilder;
 public class ActorSimulatorApplication
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		SpringApplication.run(ActorSimulatorApplication.class, args);
 
@@ -20,6 +30,21 @@ public class ActorSimulatorApplication
 		saveVehicle(client, "A70-M-622", "5", "bmw");
 	}
 
+	private static void readRoute() throws IOException
+	{
+		ClassPathResource resource = new ClassPathResource("route.txt");
+		InputStream inputStream = resource.getInputStream();
+		InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+		BufferedReader reader = new BufferedReader(streamReader);
+		List<String> res = new ArrayList<>();
+		for (String line; (line = reader.readLine()) != null;) {
+			// Process line
+			System.out.println(line);
+			res.add(line);
+		}
+
+		System.out.println(res.get(res.size()-1));
+	}
 
 	private static void saveVehicle(Client client, String vehicleId, String model, String producer) {
 		String uri = constructorURIofResource("api-gateway",  10113, "addVehicle",  "");
