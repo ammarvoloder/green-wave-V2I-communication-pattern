6968fa1796159e5c71a73170233e98b08224de30
Alija Abadzic <alija.abadzic@ibb-as.com>
2020-06-01 00:08:23 +0200

    enable scheduling

--- actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java
+++ actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java
@@ -1,29 +1,15 @@
 package at.tuwien.dse.actorsimulator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-public class ActorSimulatorApplication
-{
-    private static final Logger LOG = LoggerFactory.getLogger(ActorSimulatorApplication.class);
+@EnableScheduling
+public class ActorSimulatorApplication {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(ActorSimulatorApplication.class, args);
 
     }
