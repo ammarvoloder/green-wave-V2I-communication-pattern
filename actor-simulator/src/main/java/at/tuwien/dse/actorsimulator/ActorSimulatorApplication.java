f9b6ad9ec01ede9c701562466219218cbe39cf87
Bojana Kecman <kecman.bojana1@gmail.com>
2020-05-31 15:54:19 +0200

    Merge branch 'develop' of https://gitlab.com/alija.abadzic/dse into develop
    # Conflicts:
    #	actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java

--- actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java
+++ actor-simulator/src/main/java/at/tuwien/dse/actorsimulator/ActorSimulatorApplication.java
@@ -24,49 +24,9 @@ public class ActorSimulatorApplication
     private static final Logger LOG = LoggerFactory.getLogger(ActorSimulatorApplication.class);
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		SpringApplication.run(ActorSimulatorApplication.class, args);
+        SpringApplication.run(ActorSimulatorApplication.class, args);
 
-		Client client = ClientBuilder.newClient();
-		List<Location> coordinates = new ArrayList<>();
-
-        ClassPathResource resource = new ClassPathResource("route.txt");
-        InputStream inputStream = resource.getInputStream();
-        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        BufferedReader reader = new BufferedReader(streamReader);
-
-        for (String line; (line = reader.readLine()) != null;) {
-            String[] pointString = line.split(",");
-            double lat = Double.parseDouble(pointString[0]);
-            double lng = Double.parseDouble(pointString[1]);
-            double distance = Double.parseDouble(pointString[2]);
-            Location location = new Location(lat, lng, distance);
-            coordinates.add(location);
-        }
-
-        //STARTING SIMULATION
-        for(Location location: coordinates){
-            sendMovement(client, "AT-525-55", location);
-        }
-	}
-
-	private static void saveVehicle(Client client, String vehicleId, String model, String producer) {
-		String uri = constructorURIofResource("api-gateway",  10113, "addVehicle",  "");
-		client.target(uri)
-				.queryParam("vehicleID", vehicleId)
-				.queryParam("producer", producer)
-				.queryParam("model", model)
-				.request().header("id", producer)
-				.build("POST")
-				.invoke();
-	}
-
-	private static void getVehicles(Client client){
-		String uri = constructorURIofResource("api-gateway",  10113, "getAllVehicles",  "");
-		Response response = client.target(uri)
-				.request()
-				.build("GET")
-				.invoke();
-	}
+    }
 
     private static void sendMovement(Client client, String id, Location location) throws InterruptedException {
 	    //convert speed from km/h to m/s
@@ -76,12 +36,4 @@ public class ActorSimulatorApplication
         Thread.sleep(timeToWait);
     }
 
-    // construct and return URI of the request for all REST request in one method
-	private static String constructorURIofResource(String host, int port, String methodName, String pathParam) {
-		StringBuilder stringBuilder = new StringBuilder("http://" + host +  ":" + port + "/" + methodName);
-		if (!pathParam.isEmpty()) {
-			stringBuilder.append("/").append(pathParam);
-		}
-		return stringBuilder.toString();
-	}
 }
