8ee598acdec06c8043e7d8bb29746180f3bfbc30
Bojana Kecman <kecman.bojana1@gmail.com>
2020-06-04 20:14:31 +0200

    attached info window for markers on click

--- client/src/app/components/map/map.component.ts
+++ client/src/app/components/map/map.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TrafficLight } from 'src/app/models/traffic-light';
 import { RestService } from 'src/app/services/rest-service';
 import { element } from 'protractor';
+import { MapMarker, MapInfoWindow } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -10,19 +11,35 @@ import { element } from 'protractor';
 })
 export class MapComponent implements OnInit {
 
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
   center: google.maps.LatLng;
   coordinates = new google.maps.LatLng(48.16411, 16.34629);
   trafficLights: TrafficLight[];
   markers: google.maps.Marker[];
+  
+
 
   redLight = 'src/assets/images/red.png';
+  infoContent = '';
   
   constructor(private restService: RestService) { }
 
   ngOnInit(): void {
     this.markers = []
     this.center = this.coordinates;
-    this.getAllTrafficLights();
+    //this.getAllTrafficLights();
+    const marker = new google.maps.Marker;
+    const options = {
+      icon: this.redLight
+    }
+    marker.setPosition(this.coordinates);
+    marker.setOptions(options);
+    this.markers.push(marker);
+  }
+
+  openInfo(marker: MapMarker) {
+    this.infoContent = "Bojana Kecman";
+    this.infoWindow.open(marker);
   }
 
   getAllTrafficLights(){
@@ -36,6 +53,12 @@ export class MapComponent implements OnInit {
         }
         marker.setPosition(coordinates);
         marker.setOptions(options);
+        marker.addListener('click', function() {
+          return function() {
+            this.infoWindow.setContent("bojana");
+            this.infoWindow.open(this.map, marker);
+        }
+        });
         this.markers.push(marker);
       })
     })
