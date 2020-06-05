935c4a64c27d6993de7ed0b09a7fdd442991f67c
Ammar Voloder <ammarvoloder@gmail.com>
2020-06-05 13:01:30 +0200

    implemented full functionality for displaying traffic lights and their status

--- client/src/app/components/map/map.component.ts
+++ client/src/app/components/map/map.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild, NgZone, ElementRef, AfterViewInit } from '@angular/core';
 import { TrafficLight } from 'src/app/models/traffic-light';
 import { RestService } from 'src/app/services/rest-service';
-import { element } from 'protractor';
-import { MapMarker, MapInfoWindow, GoogleMap } from '@angular/google-maps';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 
@@ -22,27 +20,23 @@ export class MapComponent implements AfterViewInit {
     zoom: 14.5
    };
 
-
-
   trafficLights: TrafficLight[];
-  markers: google.maps.Marker[];
-  //options: google.maps.MarkerOptions[];
   ws: any;
   redLight = 'assets/images/red.png';
   greenLight = 'assets/images/green.png';
   infoContent: string;
-  trafficLightMap: Map<Number, google.maps.Marker>;
+  trafficLightMarkerMap: Map<Number, google.maps.Marker>;
 
   
   constructor(private restService: RestService) { 
-    this.trafficLightMap = new Map();
+    this.trafficLightMarkerMap = new Map();
     this.trafficLights = [];
   }
 
   ngAfterViewInit(): void {
     this.mapInitializer();
     this.getAllTrafficLights();
-    //this.initSocketConnections();
+    this.initSocketConnections();
   }
 
   mapInitializer(){
@@ -59,16 +53,9 @@ export class MapComponent implements AfterViewInit {
         marker.setPosition(coordinates);
         marker.setIcon(this.redLight);
         marker.setMap(this.map);
-        var content = `<span style="white-space: pre;">${trafficLight.fullInfo()}</span>`
-        const infoWindow = new google.maps.InfoWindow;
-        infoWindow.setContent(content);
-        marker.addListener('click', () => {
-          infoWindow.open(marker.getMap(), marker);
-          setTimeout(function(){infoWindow.close();}, 2000);
-        });
-        //this.markers.push(marker);
-        //this.trafficLights.push(new TrafficLight(element.id, element.longitude, element.latitude));
-        //this.trafficLightMap.set(element.id, marker);
+        this.addListenerToMarker(marker, trafficLight);
+        this.trafficLights.push(trafficLight);
+        this.trafficLightMarkerMap.set(trafficLight.id, marker);
       })
     });
   }
@@ -77,21 +64,15 @@ export class MapComponent implements AfterViewInit {
     return new TrafficLight(element.id, element.longitude, element.latitude);
   }
 
-/* 
-  openInfo(marker: MapMarker) {
-    let id;
-    for (let [key, value] of this.trafficLightMap.entries()) {
-      if(value.getPosition() ==  marker.getPosition()){
-        id = key;
-      }
-    }
-    let trafficLight = this.trafficLights.find(light => light.id === id);
-    console.log(trafficLight);
-    this.infoContent = trafficLight.fullInfo();
-    this.infoWindow.open(marker);
+  addListenerToMarker(marker: google.maps.Marker, trafficLight: TrafficLight){
+    var content = `<span style="white-space: pre;">${trafficLight.fullInfo()}</span>`;
+    const infoWindow = new google.maps.InfoWindow;
+    infoWindow.setContent(content);
+    marker.addListener('click', () => {
+      infoWindow.open(marker.getMap(), marker);
+      setTimeout(function(){infoWindow.close();}, 4000);
+    });
   }
-
-  
 
   initSocketConnections(){
     let ws = new SockJS("http://localhost:10113/ws");
@@ -99,25 +80,20 @@ export class MapComponent implements AfterViewInit {
     let that = this;
     this.ws.connect({}, function(frame) {
       that.ws.subscribe("/trafficLights", function(element) {
-          console.log(element.body);
           let tl = JSON.parse(element.body);
           let trafficLight = that.trafficLights.find(light => tl['trafficLightId'] === light.id);
           let index = that.trafficLights.indexOf(trafficLight);
           trafficLight.statusGreen = tl['green'];
           that.trafficLights[index] = trafficLight;
-          let marker = that.trafficLightMap.get(trafficLight.id); 
-          let marker_index = that.markers.indexOf(marker);
-          /* if (trafficLight.statusGreen) {
-            that.options = {
-              icon: that.greenLight
-            }
+          let marker = that.trafficLightMarkerMap.get(trafficLight.id); 
+          let options;
+          if (trafficLight.statusGreen) {
+            marker.setIcon(that.greenLight)
           } else {
-            that.options = {
-              icon: that.redLight
-            } 
+            marker.setIcon(that.redLight);
           }
+          that.addListenerToMarker(marker, trafficLight);
       });
     })
-  } */
-
+  } 
 }
